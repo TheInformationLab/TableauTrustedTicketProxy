7d6e84d1825b6efb98ea9843d264b3d05b1f4fc0
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2018-04-18 10:50:14 +0100

    Return result -1 as an error

--- functions/tableau.js
+++ functions/tableau.js
@@ -15,6 +15,11 @@ tableau.getTicket = function(serverUrl, username, ip, callback) {
         result: "error",
         error: error
       })
+    } else if (body == "-1") {
+      callback({
+        result: "error",
+        error: "Error -1 returned. Info to debug can be found here https://onlinehelp.tableau.com/current/server/en-us/trusted_auth_trouble_1return.htm"
+      })
     } else {
       callback({
         result: "success",
